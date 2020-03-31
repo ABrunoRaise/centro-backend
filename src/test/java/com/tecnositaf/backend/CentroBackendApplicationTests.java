4ce3531b0eac2b1ee5170c73cfa4ac31ecec922e
AndreaRaise <andrea.raise95@gmail.com>
2020-03-31 10:50:41 +0200

    fixed startup information

--- src/test/java/com/tecnositaf/backend/CentroBackendApplicationTests.java
+++ src/test/java/com/tecnositaf/backend/CentroBackendApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Esercizio1ApplicationTests {
+class CentroBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
